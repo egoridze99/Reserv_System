df541a97a44e3cac48b30dd49c7ba1581c45ef59
Egor Telnov <egor.telnov@mvideo.ru>
2022-12-27 19:01:28 +0300

    Изменит путь к апи

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,9 @@ from Schedule.blueprint import schedule_blueprint
 from Admin.blueprint import admin_blueprint
 from Certificate.blueprint import certificate_blueprint
 
-app.register_blueprint(schedule_blueprint, url_prefix='/')
+app.register_blueprint(schedule_blueprint, url_prefix='/api')
 app.register_blueprint(admin_blueprint, url_prefix='/admin/')
-app.register_blueprint(certificate_blueprint, url_prefix='/')
+app.register_blueprint(certificate_blueprint, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run()
